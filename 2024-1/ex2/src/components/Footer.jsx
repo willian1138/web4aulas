1ab98f9d7a7f9cb9c59e3b5349a2cbdbe2b1c138
willian <psilva546@hotmail.com>
2024-03-26 08:58:39 -0300

    clone

---
+++ 2024-1/ex2/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <footer>
+        <p>Todos os direitos reservados - 2024<br />
+        Desenvolvido por Junior</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ 2024-1/ex2/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <main>
+      <section>
+        <h2>HOME</h2>
+        <p>Esta é a página inicial da primeira aplicação quase completa criada na aula de Laboratório de Desenvolvimento Web.</p>
+      </section>
+    </main>
+  )
+}
+
+export default Home
